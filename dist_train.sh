e61b5684ba52d11e7e9dc19156a12119dd80d8fb
anonymous241114 <anonymous241114@outlook.com>
2024-11-14 20:47:51 +0800

    Delete dist_train.sh

--- dist_train.sh
+++
@@ -1,5 +0,0 @@
-## Pretrain
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port 6667 train_pair.py --config_file configs/pretrian_transoss.yml MODEL.DIST_TRAIN True
-
-## Finetune
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port 6667 train.py --config_file configs/hoss_transoss.yml MODEL.DIST_TRAIN True
